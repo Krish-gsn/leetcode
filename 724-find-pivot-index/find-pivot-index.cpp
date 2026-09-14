5c7885769cc6575d87881740c2c559a60a6cceb3
Krish-gsn <kgusain268@gmail.com>
2026-09-14 20:42:40 +0530

    Time: 0 ms (100.00%) | Memory: 37 MB (13.97%) - LeetSync

--- 724-find-pivot-index/find-pivot-index.cpp
+++ 724-find-pivot-index/find-pivot-index.cpp
@@ -3,18 +3,19 @@ public:
     int pivotIndex(vector<int>& nums) {
         vector<int>lsum(nums.size(),0);
         vector<int>rsum(nums.size(),0);
+        int ans=-1;
         for(int i=1;i<nums.size();i++){
             lsum[i]=lsum[i-1]+nums[i-1];
         }
-
         for(int i=nums.size()-2;i>=0;i--){
             rsum[i]=rsum[i+1]+nums[i+1];
         }
         for(int i=0;i<nums.size();i++){
             if(lsum[i]==rsum[i]){
-                return i;
+                ans=i;
+                return ans;
             }
         }
-        return -1;
+        return ans;
     }
 };
